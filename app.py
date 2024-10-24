ed59cb89f5dcd64309b822457266a2e0070d7e12
Juyuvi <juniorvicondo@gmail.com>
2024-10-24 02:17:36 -0300

    não funciona

--- app.py
+++ app.py
@@ -127,6 +127,9 @@ class HomePage(QMainWindow):
                 self.fullscreen()
 
     def on_move(self, x, y):
+
+
+        """\/\/NAO FUNCIONAAAAAAAA \/\/"""
         self.end = time()
         self.enlapsed_time = self.end - self.start_time
         self.start_time = time()
